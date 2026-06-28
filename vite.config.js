858cd93c5133d801d7a08af957e99e1d3238ac92
Li4o <ryoji1024t@gmail.com>
2026-06-28 21:46:52 +0900

    Tauri CLI install

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    Plugin: [react()]
-})
+  plugins: [react()],
+  // 👇 ここから追加
+  server: {
+    watch: {
+      ignored: ["**/src-tauri/**"], // Rustのビルドファイルを監視対象から外す
+    },
+  },
+  // 👆 ここまで追加
+});
